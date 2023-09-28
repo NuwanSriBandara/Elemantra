f2607a1e45783a38a2d0081734ab2de4306b967e
Nuwan Sriyantha Bandara <pmnsribandara@gmail.com>
2023-09-29 02:09:47 +0530

    Updated notification

--- executor.py
+++ executor.py
@@ -6,6 +6,7 @@ def executor(task, datapath):
   if(task == 'seismic'):
     print('Executing the proposed algorithm on sample seismic data')
     retrieve_output_seismic(datapath) #'./data/sample_seismic_data/data.csv'
+    print('Done!')
 
   elif(task =='bee-modify'):
     print('Executing the proposed algorithms on sample bee sound data')
@@ -13,6 +14,7 @@ def executor(task, datapath):
     audio2 = retrieve_output(datapath) # "./data/sample_bee_sounds/bee_sound_1.mp3"
     D1, D2 = generate_spectra(audio1, audio2)
     max_corr, _ = compute_corr(D1, D2)
+    print('Done!')
 
 if __name__ == "__main__":
   executor()
